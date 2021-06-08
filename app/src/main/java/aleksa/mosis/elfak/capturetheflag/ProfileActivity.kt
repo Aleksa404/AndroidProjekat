2affb65ecf5f548f8d7bc8cd2e52214d39430a19
Ugljesa Mitrovic <ugljesamitroviic@.gmail.com>
2021-06-08 19:10:49 +0200

    Picture in firebase

--- app/src/main/java/aleksa/mosis/elfak/capturetheflag/ProfileActivity.kt
+++ app/src/main/java/aleksa/mosis/elfak/capturetheflag/ProfileActivity.kt
@@ -1,22 +1,31 @@
 package aleksa.mosis.elfak.capturetheflag
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore.*
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var user : FirebaseUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val user = Firebase.auth.currentUser
+        user = Firebase.auth.currentUser as FirebaseUser
         if (user != null) {
              // User signed in
              val docRef = getInstance().collection("users").document(user.uid)
@@ -30,11 +39,12 @@ class ProfileActivity : AppCompatActivity() {
                  text_view_matches.setText(documentSnapshot.getLong("matches").toString())
                  text_view_flags.setText(documentSnapshot.getLong("flags").toString())
                  text_view_won.setText(documentSnapshot.getLong("won").toString())
+                 if(user.photoUrl != null)
+                     profile_image.setImageURI(user.photoUrl)
+                 else
+                     profile_image.setImageURI(Uri.parse("android.resource://$packageName/${R.drawable.profile_icon}"))
              }
-        } else {
-            // No user is signed in
-            Toast.makeText(
-                    this@ProfileActivity, "NEED TO SIGN IN!", Toast.LENGTH_SHORT).show()
+
         }
 
         profile_image.setOnClickListener {
@@ -47,6 +57,30 @@ class ProfileActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1000){
             profile_image.setImageURI(data?.data) // handle chosen image
+            updateProfile(data?.data)
+        }
+    }
+    private fun updateProfile(img : android.net.Uri?) {
+        user?.let { user ->
+            val photoURI = img
+            val profileUpdates = UserProfileChangeRequest.Builder()
+                    .setPhotoUri(photoURI)
+                    .build()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    user.updateProfile(profileUpdates).await()
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@ProfileActivity, "Successfully updated profile",
+                                Toast.LENGTH_LONG).show()
+                    }
+                } catch(e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@ProfileActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
         }
     }
 }
